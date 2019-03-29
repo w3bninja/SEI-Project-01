653f32dcfbf799d8d6ce9966adf92b000eb00bcb
Emma Price <ekprice01@gmail.com>
2019-03-29 14:37:12 +0000

    Updated JS to make user spaceship move left to right

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const width = 15
   const squares = []
-  let userSpaceshipIndex = 217
+  let spaceshipIndex = 217
+  let direction = 'right'
   let alienArray = [0,1,2,3,4,5,6,7,8,15,16,17,18,19,20,21,22,23,30,31,32,33,34,35,36,37,38]
 
   // Create grid
@@ -12,28 +13,57 @@ document.addEventListener('DOMContentLoaded', () => {
     const square = document.createElement('div')
     squares.push(square)
     grid.appendChild(square)
-    console.log(squares)
   }
 
 
   // User spaceship ======================================
   // Create user spaceship
-  squares[userSpaceshipIndex].classList.add('spaceship')
+  squares[spaceshipIndex].classList.add('spaceship')
+  squares[spaceshipIndex].classList.add('data-direction', direction)
+
+
 
   // Create function to move user spaceship
 
   function moveSpaceship() {
-
+    // find the square with the class of spaceship
+    const spaceship = squares.find(square => square.classList.contains('spaceship'))
+    // remove the class of spaceship from that square
+    spaceship.classList.remove('spaceship')
+    // add the class of player to square the player should move to
+    squares[spaceshipIndex].classList.add('spaceship')
   }
+
+  // Add event listener to move user moveSpaceship
+
+  document.addEventListener('keydown', (e) => {
+    switch(e.keyCode) {
+      case 37:
+        //left
+        if(spaceshipIndex % width > 0) {
+          spaceshipIndex--
+          moveSpaceship()
+        }
+        break
+      case 39:
+        //right
+        if(spaceshipIndex % width < width -1) {
+          spaceshipIndex++
+          moveSpaceship()
+        }
+        break
+    }
+  })
 
 
   // Aliens ==============================================
-  // Create alien alien array
 
+  // Create alien array
   alienArray.forEach(alien => {
     console.log('alien array foreach', squares[alien])
     squares[alien].classList.add('activeAlien')
   })
+  console.log(alienArray)
 
 
   // let alienArray = squares[alienArray].classList.add('activeAlien')
