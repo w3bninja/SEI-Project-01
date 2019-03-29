70ecc980c1ca177b132ee505d53afff3785f3c96
Emma Price <ekprice01@gmail.com>
2019-03-29 11:35:34 +0000

    Created 15x15 grid in JS with minimal styling

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log('JS loaded')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  const width = 15
+  const squares = []
+
+// Create grid
+  for(let i = 0; i < width * width; i++) {
+    const square = document.createElement('DIV')
+    squares.push(square)
+    grid.appendChild(square)
+  }
+//
+// // Select aliens
+//  const alienArray = [
+//
+//  ]
+
+  // Loop over array of aliens
+//  alienArray.forEach(element, index, array) => {
+//    alienArray.classList.add('active')
+//
+//  })
+//
+//  classList.add('activeAlien')
+// }
+
+
+})
